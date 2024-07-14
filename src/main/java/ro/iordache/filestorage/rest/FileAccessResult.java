2dafcc1863356bb9ea80d2349c51212ee74a96e6
Florin Iordache <>
2024-07-14 11:43:38 +0300

    removing unused code

--- src/main/java/ro/iordache/filestorage/rest/FileAccessResult.java
+++ src/main/java/ro/iordache/filestorage/rest/FileAccessResult.java
@@ -1,47 +1,29 @@
 package ro.iordache.filestorage.rest;
 
-import java.net.URI;
-
 import org.springframework.core.io.InputStreamResource;
 
 /**
  * File access operation result
  */
 public class FileAccessResult extends RESTApiResult {
-    
-    // result URI
-    private URI uri;
-    
     // result InputStreamResource
     private InputStreamResource inputStreamResource;
     
-    private FileAccessResult(int type, URI uri, InputStreamResource is) {
+    private FileAccessResult(int type, InputStreamResource is) {
         this.type = type;
-        this.uri = uri;
         this.inputStreamResource = is;
-    }
-    
-    public URI getURI() {
-        return this.uri;
     }
     
     public InputStreamResource getInputStream() {
         return this.inputStreamResource;
     }
     
-    public static FileAccessResult build(int type, URI uri, InputStreamResource is) {
-        return new FileAccessResult(type, uri, is);
-    }
-    
     public static FileAccessResult build(int type, InputStreamResource is) {
-        return new FileAccessResult(type, null, is);
+        return new FileAccessResult(type, is);
     }
-    
-    public static FileAccessResult build(int type, URI uri) {
-        return new FileAccessResult(type, uri, null);
-    }
+
     
     public static FileAccessResult build(int type) {
-        return new FileAccessResult(type, null, null);
+        return new FileAccessResult(type, null);
     }
 }
